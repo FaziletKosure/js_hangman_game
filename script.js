253fd8aba8172270895ab72472400699f1644894
FaziletKosure <faziletkosure1@gmail.com>
2020-09-28 18:13:20 +0200

    add wrong letters &play again

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const wordEl=document.getElementById("word");
 const wrongLettersEl=document.getElementById("wrong-letters");
-const playAgainBtn=document.getElementById("play-again");
+const playAgainBtn=document.getElementById("play-button");
 const popup=document.getElementById("popup-container");
 const notification=document.getElementById("notification-container");
 const finalMessage=document.getElementById("final-message");
@@ -9,10 +9,10 @@ const figureParts=document.querySelectorAll('.figure-part');
 
  const words=['application','programming','interface','wizard'];
 
- const selectedWord=words[Math.floor(Math.random()*words.length)];
+ let selectedWord=words[Math.floor(Math.random()*words.length)];
  console.log(selectedWord);
-//  const correctLetters=[];
- const correctLetters=["w","i","z","a","r","d"];
+ const correctLetters=[];
+//  const correctLetters=["w","i","z","a","r","d"];
  const wrongLetters=[];
 //Show hidden word
  function displayWord() {
@@ -27,8 +27,26 @@ const figureParts=document.querySelectorAll('.figure-part');
      
  }
 // Update the Wrong Letters
-function updateWrongLettersEl() {
+function updateWrongLetterttsEl() {
+    //Display wrong letters
     console.log('Update wrong');
+    wrongLettersEl.innerHTML=`${wrongLetters.length>0? '<p>Wrong</p>':""} ${wrongLetters.map(letter=>`<span>${letter}</span>`)}`
+    //Display parts
+    figureParts.forEach((part,index)=>{
+        const errors=wrongLetters.length;
+        if (index<errors) {
+            part.style.display="block";
+            
+        }else{
+            part.style.display="none";
+        }
+    });
+    // Check if lost
+    if (wrongLetters.length===figureParts.length) {
+        finalMessage.innerHTML="Unfortunately you lost. &#128524;";
+        popup.style.display='flex';
+        
+    }
     
 }
 //  show Notification
@@ -57,12 +75,23 @@ window.addEventListener("keydown",e=>{
         }else{
             if (!wrongLetters.includes(letter)) {
                 wrongLetters.push(letter);
-                updateWrongLettersEl()
+                updateWrongLetterttsEl()
                 
             }
 
         }
     }
 
+})
+// Restart game and play again
+playAgainBtn.addEventListener("click",()=>{
+    // Empty arrays
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+    selectedWord=words[Math.floor(Math.random()*words.length)];
+   
+    displayWord();
+    updateWrongLetterttsEl();
+    popup.style.display='none';
 })
  displayWord();
